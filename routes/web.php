fb1d5cfc0019f74b8c593491a6a7e5f5c475623f
WoroAnggitaSari <woroanggitasari2@gmail.com>
2019-09-04 17:31:34 +0700

    update web.php lengkap

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,23 @@
 |
 */
 
-<<<<<<< HEAD
+
 Route::get('/home', function () {
     return view('index_pilihan');
 =======
 // Route::get('/', function () {
-<<<<<<< HEAD
+
 //     return view('welcome');
 // });
 
 Route::get('/provinsi', function () {
     return view('index_provinsi');
->>>>>>> 61de0ec7d7d17ade823a5c0f95dfdfe784c4eea9
 });
 
 Route::get('/','ItemController@index');
 Route::post('/item/tambah','ItemController@tambah');
 Route::get('/item/ubah/{id}','ItemController@edit');
 Route::post('/item/update','ItemController@ubah');
-=======
 //     return view('index_lapak');
 // });
 
@@ -45,13 +43,8 @@ Route::get('/provinsi/edit/{id}','ProvinsiController@edit');
 Route::post('/provinsi/update','ProvinsiController@update');
 
 
-
-<<<<<<< HEAD
 Route::get('/index_pilihan','PilihanController@index');
 Route::post('/pilihan/tambah','PilihanController@tambah');
 Route::get('/pilihan/ubah/{id}','PilihanController@ubah');
 Route::post('/pilihan/menambahkan','PilihanController@menambahkan');
 
-=======
->>>>>>> b52cac8ba70417f6f8329d9f78374e831415fbbf
->>>>>>> 61de0ec7d7d17ade823a5c0f95dfdfe784c4eea9
